7da5d5868a981cae5b758bed175031fb3a5bd396
Nidhi <nidhikanwar45@gmail.com>
2022-09-12 10:24:13 +0530

    Readme file added

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+const Header =()=>{
+    return(
+        <div className= "header">
+            <div className="cart-div">
+                <span>0</span>
+                <h1>hello</h1>
+                <img src="https://img.freepik.com/premium-vector/vector-shopping-cart-icon-paper-sticker-with-shadow-colored-shopping-symbol-isolated_118339-1774.jpg?w=2000" />
+            </div>
+        </div>
+    )
+}
